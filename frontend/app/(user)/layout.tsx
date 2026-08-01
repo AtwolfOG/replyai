606fb6756abad9800079d27c98bfe7b84f28b4c6
AtwolfOG <ayokunletestimony702@gmail.com>
2026-08-01 23:38:37 +0100

    refactor: remove page transitions and add mobile bottom padding to layout

--- frontend/app/(user)/layout.tsx
+++ frontend/app/(user)/layout.tsx
@@ -1,14 +1,11 @@
 import { BottomNavBar, TopNavbar } from "@/components/navbar";
-import Transition from "@/components/pageTransition"
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
         <main className="flex flex-col min-h-screen">
             <TopNavbar />
-            <div className="relative flex-1 px-(--space-8) py-(--space-4)">
-                <Transition>
-                    {children}
-                </Transition>
+            <div className="relative flex-1 px-(--space-8) py-(--space-4) max-sm:pb-(--space-20)">
+                {children}
             </div>
             <BottomNavBar />
         </main>
